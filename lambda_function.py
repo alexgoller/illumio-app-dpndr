f411ef935838d99bc06ed244515e4bd5be00d7b5
alexgoller <83632450+alexgoller@users.noreply.github.com>
2024-07-27 16:28:35 +0200

    CORS testing

--- lambda_function.py
+++ lambda_function.py
@@ -214,10 +214,7 @@ def lambda_handler(event, context):
 		return {
 			'statusCode': 200,
 			'body': json.dumps({'image_url': url}),
-			'headers': {
-				'Access-Control-Allow-Origin': '*',
-				'Content-Type': 'application/json'
-			}
+			'headers': headers
 		}
 	except Exception as e:
 		return {
